b1c182f1a9c057ed9ecdabb384de4c3de4c7c35e
vvarsema-prog <vvarsema@gmail.com>
2026-03-19 13:20:12 +0400

    js davaleba

---
+++ js/loop.js
@@ -0,0 +1,40 @@
+// for (jondo=1; jondo < 21 ; jondo++){
+//     console.log(jondo);
+//     if(jondo %2 == 1)
+//     console.log(jondo);
+    
+// }
+// let a=3;
+// while(a<20){
+//     if(a%2==0 && a!=0){
+//         console.log(a);
+//     }
+//      a++;        
+// }
+
+
+
+
+// let a = 0
+// while (a<30){
+//     if(a%5==0 && a%2==0){
+//         console.log("ok");
+        
+//     }
+//     else{
+//         console.log("dinate");
+        
+//     }
+//     a++;
+// }
+
+for (bondo=0; bondo <100 ;bondo++){
+    console.log(bondo);
+}
+let a = 0;
+    for(i=1 ; i<=100;i++){
+        a+=i
+    }
+    console.log(a);
+    
+    
